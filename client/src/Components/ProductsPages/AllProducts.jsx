b1049d62205a11fa6b6379329a8b06346de94b24
Ntshembo <juniorWebProjects@gmail.com>
2020-10-15 12:35:25 +0200

    Bug Fix on fetching data

--- client/src/Components/ProductsPages/AllProducts.jsx
+++ client/src/Components/ProductsPages/AllProducts.jsx
@@ -3,48 +3,48 @@ import axios from "axios";
 import "../../StyleSheet/AllProducts.css";
 import { Link } from "react-router-dom";
 const AllProducts = () => {
-  const url = "/api/all-products";
+  const url = `/api/all-products`;
   const [products, setProducts] = useState();
- 
-  const [mounted, setMounted] = useState(true)
+
+  const [mounted, setMounted] = useState(true);
   useEffect(() => {
+    const loadData = async () => {
+      const { data } = await axios.get(url);
 
-    const loadData = async()=>{
-      const {data} = await axios.get(url)
-
-      if (mounted){
-        setProducts(data)
+      if (mounted) {
+        setProducts(data);
       }
-    }
-    loadData()
-    return ()=>{
-      setMounted(false)
-    }
-
+    };
+    loadData();
+    return () => {
+      setMounted(false);
+    };
   }, [mounted]);
 
   return (
     <div className="All__availProductsContainer">
-      {products && <div className="AllProducts__container">
-        <h3>ALL PRODUCTS</h3>
-        <div className="Allproducts__container">
-          {products.map((product) => {
-            return (
-              <div className="all_products" key={product._id}>
-                <Link
-                  to={`/product-detail/${product._id}`}
-                  className="Router__link"
-                >
-                  <img
-                    src={product.image}
-                    alt="Product not visible? Contact please"
-                  />
-                </Link>
-              </div>
-            );
-          })}
+      {products && (
+        <div className="AllProducts__container">
+          <h3>ALL PRODUCTS</h3>
+          <div className="Allproducts__container">
+            {products.map((product) => {
+              return (
+                <div className="all_products" key={product._id}>
+                  <Link
+                    to={`/product-detail/${product._id}`}
+                    className="Router__link"
+                  >
+                    <img
+                      src={product.image}
+                      alt="Product not visible? Contact please"
+                    />
+                  </Link>
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div>}
+      )}
     </div>
   );
 };
