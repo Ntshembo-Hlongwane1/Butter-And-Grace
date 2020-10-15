df8e2b8a749d132ec95a7cbccf09be2e86e30f32
Ntshembo <juniorWebProjects@gmail.com>
2020-10-15 12:21:29 +0200

    Bug Fix on fetching data

--- client/src/axios.js
+++ client/src/axios.js
@@ -5,8 +5,9 @@ const url = process.env.PORT
   ? "https://butter-and-grace.herokuapp.com:" + port
   : "http://localhost:5000";
 
+const production_url = "https://butter-and-grace.herokuapp.com:" + port;
 const instance = axios.create({
-  baseURL: url,
+  baseURL: production_url,
 });
 
 export default instance;
